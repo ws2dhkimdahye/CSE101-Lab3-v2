164f70af6252167ab89a6aa24bca70d8e1dbd908
Francois Rameau <rameau.fr@gmail.com>
2024-03-18 15:54:35 +0900

    first commit

---
+++ Lab3-FctAndCond.py
@@ -0,0 +1,34 @@
+
+# Conversion function placeholders
+# Task 1: Create two functions to convert between pyeong and square meters.
+
+# Value calculation function placeholder
+# Task 2: Create a function to calculate the value of an apartment based on size in m2, location, access quality, and view.
+#    What parameters will this function need?
+#    How will each parameter affect the apartment's value?
+#    Remember to consider different factors like location premium, access quality, and the view premium.
+
+# your main function here
+def main():
+    
+    # Property 1 specifications
+    surface_pyeong_1 = 25
+    location_1 = "Gangnam"
+    access_1 = "Good" # Poor / Fair / Good / Very Good
+    view_1 = True # view on the Han River?
+    
+    # Property 2 specifications
+    surface_pyeong_2 = 45
+    location_2 = "Other"
+    access_2 = "Poor" # Poor / Fair / Good / Very Good
+    view_2 = False # view on the Han River?
+    
+    # Task 1: Convert pyeong to m2
+
+    # Task 2: Calculate properties' values
+    
+    # Task 3: Find the cheapest one and print
+
+# Call the main function here!
+if __name__ == "__main__": # <-- # checks if being run directly (not imported)
+    main()
---
+++ README.md
@@ -0,0 +1,5 @@
+# Lab 3: Functions and conditional statements
+
+All the necessary instructions can be found in the slides shared on Brightspace
+
+Good Luck!
